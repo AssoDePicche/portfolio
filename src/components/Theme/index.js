59bc00e9630c3c3b94f53b64e25bb4386dc957eb
AssoDePicche <samuelprado730@gmail.com>
2023-02-22 12:56:10 -0300

    feat: add theme component

---
+++ src/components/Theme/index.js
@@ -0,0 +1,27 @@
+const Theme = {
+  colors: {
+    neutral: {
+      dark: "#232323",
+      light: "#f5f5f5",
+    },
+  },
+  typo: {
+    fontFace: {
+      main: "Poppins, sans-serif",
+    },
+    fontSize: {
+      base: "1.6rem",
+      large: "3.2rem",
+      extraLarge: "6.4rem",
+    },
+    spacing: {
+      base: 1.5,
+    },
+  },
+  spacing: {
+    base: "1.6rem",
+    extraLarge: "5.6rem",
+  },
+};
+
+export default Theme;
