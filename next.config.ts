ed4bf5c1a1600e4d5efb9823c9169dc620a57b82
AssoDePicche <samuelprado730@gmail.com>
2025-05-01 19:19:20 -0300

    feat: components alias + github avatar remote pattern

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ const path = require('path');
 module.exports = {
   webpack: (config) => {
     config.resolve.alias['@styles'] = path.join(__dirname, 'source/styles');
+
+    config.resolve.alias['@components'] = path.join(__dirname, 'source/components');
+
     return config;
   },
+  images: {
+    remotePatterns: [
+      new URL('https://avatars.githubusercontent.com/u/86676526?v=4')
+    ]
+  }
 };
